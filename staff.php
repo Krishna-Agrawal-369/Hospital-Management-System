102db1db2b49d196cd8414f113df615a6dc6b7de
Krishna-Agrawal-369 <76277439+Krishna-Agrawal-369@users.noreply.github.com>
2021-07-22 15:34:38 +0530

    Create staff.php

---
+++ staff.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<div class="jumbotron" style="background-color:deepskyblue;
+    background-size: cover; height:1000px;">
+<div class="container">
+    <div class="card">
+        <div class="card-body" style="background-color:limegreen;color:#ffffff;">
+        <div class="row">
+        <div class="col-md-1">
+            <a href="admin-panel.php" class="btn btn-light">Go Back</a>
+        </div> 
+        <div class="col-md-3"> <h3>Staff Details</h3></div>
+        <div class="col-md-8">
+        <form class="form-group" action="func.php" method="post">
+                <div class="row">
+                    <div class="col-md-10"><input type="text" name="search" class="form-control"></div>
+                    <div class="col-md-2"><input type="submit" name="patient_search" class="btn btn-light" value="Search"></div>
+                </div>
+        </form>
+        </div>
+        </div></div>
+        <div class="card-body" style="background-color:limegreen;color:#ffffff;">
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">First Name</th>
+      <th scope="col">Last Name</th>
+      <th scope="col">Shift</th>
+      <th scope="col">Contact no</th>
+      <th scope="col">Work Type</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>Vipul</td>
+      <td>Sharma</td>
+      <td>9AM-9PM</td>
+      <td>9131614016</td>
+      <td>Wardboy</td>
+    </tr>
+    <tr>
+      <td>Ritika</td>
+      <td>Sharma</td>
+      <td>10AM-10PM</td>
+      <td>9131999999</td>
+      <td>Receptionist</td>
+    </tr>
+    <tr>
+      <td>Nitya</td>
+      <td>Agrawal</td>
+      <td>8PM-8AM</td>
+      <td>9131613999</td>
+      <td>Receptionist</td>
+    </tr>
+    <tr>
+      <td>Palash</td>
+      <td>Bajpai</td>
+      <td>10AM-10PM</td>
+      <td>9133413016</td>
+      <td>Wardboy</td>
+    </tr>
+    <tr>
+      <td>Ram</td>
+      <td>Sharma</td>
+      <td>9PM-9PM</td>
+      <td>9133413033</td>
+      <td>Wardboy</td>
+    </tr>
+    <tr>
+      <td>Ajit</td>
+      <td>Jain</td>
+      <td>10PM-10AM</td>
+      <td>9130413016</td>
+      <td>Wardboy</td>
+    </tr>
+    <tr>
+      <td>Sanjana</td>
+      <td>Kohli</td>
+      <td>9AM-9PM</td>
+      <td>9133413016</td>
+      <td>Nurse</td>
+    </tr>
+    <tr>
+      <td>Ankita</td>
+      <td>Goyal</td>
+      <td>10PM-10AM</td>
+      <td>9133413016</td>
+      <td>Nurse</td>
+    </tr>
+    <tr>
+      <td>Saloni</td>
+      <td>Dani</td>
+      <td>9AM-8PM</td>
+      <td>9133413016</td>
+      <td>Nurse</td>
+    </tr>
+  </tbody>
+</table>
+</div>
+</div>
+
+
+</div>
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    
+</body>
+</html>
