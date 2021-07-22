84a14df0d3f45b62378ab59c228b58eee5fdcc40
Krishna-Agrawal-369 <76277439+Krishna-Agrawal-369@users.noreply.github.com>
2021-07-22 15:30:24 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hospital Management System</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" 
+    integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<style type="text/css">
+    #ab1:hover{cursor:pointer;}
+</style>
+<body style="background-color:greenyellow; no-repeat;background-size:cover;">
+    <div class="container" style="width:400px; margin-top:100px;">
+    <div class="card">
+    <img src="images1/h2.jpg" class="card-img-top">
+    <div class="card-body">
+        <form class="form-group" action="func.php" method="post">
+            <label>Username :</label><br>
+            <input type="text" name="username" class="form-control" placeholder="enter username"><br>
+            <label>Password :</label><br>
+            <input type="password" name="password" class="form-control" placeholder="enter password"><br>
+            <input type="submit" name="login_submit" class="btn btn-primary">
+        </form>
+    </div>
+    </div>
+    </div>
+    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
